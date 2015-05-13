01c4d27f143ccb2dccafab933c2c5ec0279f9898
pareshb <paresh.bhavsar@clairvoyantsoft.com>
2015-05-13 15:11:19 -0700

    adding math function for understanding node.js modules

---
+++ mul.js
@@ -0,0 +1,10 @@
+mul = function mul(x,y){
+	return x*y;
+}
+
+add = function add(x,y){
+	return x+y;
+}
+
+exports.m = mul;
+exports.a = add;
---
+++ server.js
@@ -0,0 +1,4 @@
+math = require('./mul');
+console.log(math.m(1,2));
+console.log(math.a(1,2));
+
